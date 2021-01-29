85a15d9888f069a9f3ebca40f8e66ba00bc2818b
dav-medcor <medina.cortes2@gmail.com>
2021-01-29 10:13:32 +0100

    Ejercicio 3 completo

---
+++ Ejercicio3/index.js
@@ -0,0 +1,16 @@
+//Ejercicio con array
+console.log ("probando");
+const array = ["hola", 2, 5, "adios"];
+console.log (array[1] > array[2]);
+console.log (array[2] > array [1]);
+console.log ("5 es mayor que 2");
+const suma = array[1] + array[2];
+const resta = array[1] - array[2];
+const multiplicacion = array [1] * array[2];
+const division = array[1] / array[2];
+const resto = array[1] % array[2];
+console.log ("El resultado de la suma es", suma);
+console.log ("El resultado de la resta es", resta);
+console.log ("El resultado de la multiplicación es", multiplicacion);
+console.log ("El resultado de la división es", division);
+console.log ("El resto de la división es", resto);
