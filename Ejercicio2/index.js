9f3a7eb03c7cdec6ed804184dfe047ca4bf2d1b2
dav-medcor <medina.cortes2@gmail.com>
2021-01-29 10:03:51 +0100

    Ejercicio2 completo

---
+++ Ejercicio2/index.js
@@ -0,0 +1,19 @@
+console.log ("Probando")
+//Fecha de cumpleaños
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", 
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+const david = {
+    dia: 2,
+    mes: meses[11],
+};
+
+const celta = {
+    dia: 20,
+    mes: meses[3],
+};
+
+console.log ("El cumpleaños de David es el", david.dia, "de", david.mes);
+console.log ("El mes siguiente al cumple de David es", meses[0]);
+console.log ("El cumpleaños de los Celtas es el", celta.dia, "de", celta.mes);
+console.log ("El mes siguiente al cumple de los Celtas es el", meses[4]);
